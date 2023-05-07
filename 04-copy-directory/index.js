ba239021493b702ad4c862994e5fdbe187b948c3
unknown <timur.dev@mail.ru>
2023-05-08 00:25:00 +0300

    feat: resolve copy dir task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,32 @@
+const path = require('path');
+const fs = require('fs');
 
+const url = path.join(__dirname, 'files');
+const urlCopy = path.join(__dirname, 'files-copy');
+
+// Create dir
+fs.mkdir(urlCopy, { recursive: true }, (err) => {
+    if (err) console.log(err.message);
+});
+
+// Remove all old files from copy
+fs.readdir(urlCopy, (err, files) => {
+    if (err) return console.log(err.message);
+    for (let file of files) {
+        fs.unlink(path.join(urlCopy, file), err => {
+            if (err) throw err;
+        });
+    }
+});
+
+// Read original dir, then copy
+fs.readdir(url, { withFileTypes: true }, (err, files) => {
+    if (err) return console.log(err.message);
+    for (let file of files) {
+        let orig = path.join(url, file.name);
+        let copy = path.join(urlCopy, file.name);
+        fs.copyFile(orig, copy, (err) => {
+            if (err) console.log(err.message);
+        });
+    }
+});
